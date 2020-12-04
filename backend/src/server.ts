6b2569994e8fb12bea8c226d1547c69fea00d42b
Lauro Miguel <lauromvs@hotmail.com>
2020-12-04 15:37:53 -0300

    feat: route to serve static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,12 +5,15 @@ import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes/index';
+import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
